1bdbc519a8146a7d4fa49cfbe711ae721ffec971
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-06-15 13:26:20 +0300

    fix deploy mistake

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import {path }from "node:path";
+import path from "node:path";
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
