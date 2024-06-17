ddf7d9122012aa0ae9c316ff8122adcad7a9781c
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-17 20:48:46 +0200

    anchor links

--- app/components/containers/footer/footer.tsx
+++ app/components/containers/footer/footer.tsx
@@ -2,7 +2,7 @@ import "./footer.scss";
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer" id="anchor-footer">
       <div className="container">Footer</div>
     </footer>
   );
