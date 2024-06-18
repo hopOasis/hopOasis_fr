f003bb054bba11d7a5ff28516bcb4e13e2592159
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-18 15:39:12 +0200

    refactoring proposals-section

--- app/components/containers/proposals/proposals.tsx
+++ app/components/containers/proposals/proposals.tsx
@@ -1,6 +1,7 @@
-import { ProposalCard } from '../../ui/ProposalCard/ProposalCard';
-import Section from '../../ui/section/section';
-import './proposals.scss';
+import { proposals } from "@/app/static/proposals";
+import { ProposalCard } from "../../ui/ProposalCard/ProposalCard";
+import Section from "../../ui/section/section";
+import "./proposals.scss";
 
 export default function Proposals() {
   return (
@@ -8,33 +9,9 @@ export default function Proposals() {
       <div className="proposal">
         <h2 className="title">Пропозиції від нашої команди</h2>
         <div className="proposal-container">
-          <div className="proposal-block">
-            <ProposalCard
-              imageUrl={'/olena-proposal.png'}
-              employee={'Олeна, технолог'}
-              proposalText={
-                'Подобається пиво сорту IPA (India Pale Ale) через його характерний гіркий смак, який надається використанням великої кількості хмелю. Також IPA часто має яскравий аромат хмелю, що робить його цікавим для любителів насичених смаків.'
-              }
-              beerUrl={'/beer/32334'}
-            />
-            <ProposalCard
-              imageUrl={'/andriy-proposal.png'}
-              employee={'Андрій, технолог'}
-              proposalText={
-                'Подобається пиво сорту IPA (India Pale Ale) через його характерний гіркий смак, який надається використанням великої кількості хмелю. Також IPA часто має яскравий аромат хмелю, що робить його цікавим для любителів насичених смаків.'
-              }
-              beerUrl={'/beer/32334'}
-            />
-          </div>
-          <ProposalCard
-            imageUrl={'/oleh-proposal.png'}
-            employee={'Олег, технолог'}
-            proposalText={
-              'Подобається пиво сорту IPA (India Pale Ale) через його характерний гіркий смак, який надається використанням великої кількості хмелю.'
-            }
-            beerUrl={'/beer/32334'}
-            isColumnDirection={true}
-          />
+          {proposals.map((props) => (
+            <ProposalCard {...props} />
+          ))}
         </div>
       </div>
     </Section>
---
+++ app/components/ui/ProposalCard/types.ts
@@ -0,0 +1,8 @@
+import { StaticImageData } from "next/image";
+
+export type Props = {
+  img: StaticImageData;
+  employee: string;
+  proposalText: string;
+  link: string;
+};
