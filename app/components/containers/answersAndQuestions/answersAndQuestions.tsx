cdfe4037e3008f0b70587a9c3d9bf7a5ed44a65d
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-19 18:16:37 +0200

    refactoring AnswAndQuest

--- app/components/containers/answersAndQuestions/answersAndQuestions.tsx
+++ app/components/containers/answersAndQuestions/answersAndQuestions.tsx
@@ -1,15 +1,15 @@
-'use client';
-import { questions } from '@/app/static/ansert-and-questions';
-import Section from '../../ui/section/section';
-import './answersAndQuestions.scss';
-import { QuestionsList } from './question-list/QuestionItem';
+"use client";
+import { questions } from "@/app/static/ansert-and-questions";
+import Section from "../../ui/section/section";
+import "./answersAndQuestions.scss";
+import { QuestionsList } from "./question-list/QuestionItem";
 
 export default function AnswersAndQuestions() {
   return (
     <Section>
       <div className="answers-and-questions">
         <h2 className="title">Поширені питання</h2>
-        <QuestionsList questions={questions} />
+        <QuestionsList />
       </div>
     </Section>
   );
--- app/components/containers/answersAndQuestions/question-list/QuestionItem.tsx
+++ app/components/containers/answersAndQuestions/question-list/QuestionItem.tsx
@@ -1,42 +1,31 @@
-import { QuestionButton } from '@/app/components/ui/buttons/buttons';
-import React, { useState } from 'react';
-import { Question } from './type';
-import './questions-list.scss';
-type Props = {
-  questions: Question[];
-};
+import { QuestionButton } from "@/app/components/ui/buttons/buttons";
+import React, { useState } from "react";
+import "./questions-list.scss";
+import { questions } from "@/app/static/ansert-and-questions";
 
-export const QuestionsList: React.FC<Props> = ({ questions }) => {
-  const [ids, setIds] = useState<number[]>([]);
-
-  const onHandleClick = (id: number) => {
-    if (ids?.includes(id)) {
-      setIds((currentIds) =>
-        currentIds.filter((currentId) => currentId !== id),
-      );
-      return;
-    }
-    setIds((currentIds) => [...currentIds, id]);
-  };
+const Accordion = ({ id, question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <ul className="questions-list">
-      {questions.map(({ id, question }) => (
-        <li className="questions-list__item" key={id}>
-          <div className="questions-list__item-container">
-            <p>{`${id}. ${question}`}</p>
-            <QuestionButton
-              isActive={ids.includes(id)}
-              onClick={() => onHandleClick(id)}
-            />
-          </div>
-          <p className={ids.includes(id) ? 'answer--active' : 'answer'}>
-            {
-              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-            }
-          </p>
-        </li>
+    <details
+      key={id}
+      className="questions-list__item"
+      onToggle={(e) => setIsOpen(e.target.open)}
+    >
+      <summary className="questions-list__item-container">
+        {`${id}. ${question}`}
+      </summary>
+      <p className={isOpen ? "animation" : undefined}>{answer}</p>
+    </details>
+  );
+};
+
+export const QuestionsList = () => {
+  return (
+    <div className="questions-list">
+      {questions.map((props) => (
+        <Accordion {...props} />
       ))}
-    </ul>
+    </div>
   );
 };
