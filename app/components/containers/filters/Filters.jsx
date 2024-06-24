bd8e3c0efd7ad1c756bd5a51631a01d87391d077
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-24 17:27:18 +0200

    filters

--- app/components/containers/filters/Filters.jsx
+++ app/components/containers/filters/Filters.jsx
@@ -8,12 +8,19 @@ import { useSearchParams } from "next/navigation";
 export default function Filters() {
   const searchParams = useSearchParams();
   const filter = searchParams.get("filter");
+
+  const activeClass = (id) => {
+    if (filter === id || (filter === null && id === "all")) {
+      return "active";
+    }
+  };
+
   return (
     <ul className="shop-filters">
       {filters.map(({ id, name }) => (
         <li key={id}>
           <Link
-            className={filter === id ? "active" : undefined}
+            className={activeClass(id)}
             href={{
               pathname: routes[2].href,
               query: `filter=${id}`,
