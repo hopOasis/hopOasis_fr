83fca6c4620ffa0e2a3882ecb905b4debefd7c14
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-06-25 17:29:08 +0300

    wrape Filter component in Suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={montserrat.className}>
         <Header />
+
         {children}
         <Footer />
         <Suspense>
