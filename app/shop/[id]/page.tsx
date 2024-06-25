0bd7347ddea1978b1e00eebb8d02b6c143bdc2dc
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-25 19:16:37 +0200

    add image upd cart button

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -8,28 +8,32 @@ import Title from "./Title";
 import DeliveryText from "@/app/components/ui/DeliveryText/DeliveryText";
 import Icons from "@/app/components/ui/icons/icons";
 import { CardButton } from "@/app/components/ui/buttons/buttons";
+import Image from "next/image";
 
 export default function SinglePage() {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
     <main className="single-page">
-      <h3>I am a beer</h3>
       <Section>
-        <p className="accent">{beer.priceLarge} грн.</p>
-        <Rating onChange={(value) => console.log(value)} />
-        <CardButton
-          onClick={() => {
-            console.log("button-id", beer.id);
-          }}
-        />
-        <button
-          className="single-page__fav-button"
-          type="button"
-          onChange={() => console.log(beer.id)}
-        >
-          <Icons name="heart" stroke="#292929" /> Додати до обраного
-        </button>
+        <Image src={beer.image} alt={beer.name} width={628} height={431} />
+        <div className="single-page__description-block">
+          <h1>{beer.name}</h1>
+          <p className="accent">{beer.priceLarge} грн.</p>
+          <Rating onChange={(value) => console.log(value)} />
+          <CardButton
+            onClick={() => {
+              console.log("button-id", beer.id);
+            }}
+          />
+          <button
+            className="single-page__fav-button"
+            type="button"
+            onChange={() => console.log(beer.id)}
+          >
+            <Icons name="heart" stroke="#292929" /> Додати до обраного
+          </button>
+        </div>
       </Section>
       <Section>
         <Title
