f19b45d88b848e6e00cf03e4999e43ed9ff43774
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-06-27 19:31:30 +0300

    create basic slider

--- app/components/ui/modals/WeeklyProductsModal.tsx
+++ app/components/ui/modals/WeeklyProductsModal.tsx
@@ -1,18 +1,19 @@
 import { beer } from '@/app/static/bear';
 import Gallery from '../../containers/gallery/gallery';
 import Card from '../card/Card';
+import { CardSlider } from '../slider/CardSlider';
 
 export default function WeeklyProductsModal() {
+  const products = Array(8)
+    .fill(1)
+    .map(() => {
+      const id = crypto.randomUUID();
+      return { ...beer, id };
+    });
   return (
     <section className="cart-modal__container cart-modal__section ">
       <p className="cart-modal__title">Рекомендовані товари</p>
-      <Gallery>
-        {Array(3)
-          .fill(1)
-          .map(() => (
-            <Card {...beer} key={beer.id} />
-          ))}
-      </Gallery>
+      <CardSlider products={products} slidesPerView={3} />
     </section>
   );
 }
