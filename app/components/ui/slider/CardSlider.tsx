125c75f188fad8d83b746ed5e0a80ee625c42ed4
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-28 21:27:19 +0200

    add swiper navigation

--- app/components/ui/slider/CardSlider.tsx
+++ app/components/ui/slider/CardSlider.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./cardslider.scss";
 import { SwiperContainer, register } from "swiper/element/bundle";
+import Icons from "../icons/icons";
 
 type Props = {
   products: React.ReactElement[];
@@ -13,25 +14,53 @@ export const CardSlider: React.FC<Props> = ({
   slidesPerView = 4,
 }) => {
   const swiperElRef = useRef<SwiperContainer>(null);
+  const [isActiveIdx, setIsActiveIdx] = useState(0);
 
   useEffect(() => {
     register();
     if (swiperElRef.current) {
-      Object.assign(swiperElRef.current, {});
+      Object.assign(swiperElRef.current, {
+        on: {
+          slideChange(e) {
+            setIsActiveIdx(e.activeIndex);
+            e.isEnd && setIsActiveIdx(-1);
+          },
+        },
+      });
       swiperElRef.current.initialize();
     }
   }, []);
 
   return (
-    <swiper-container
-      slides-per-view={slidesPerView}
-      space-between={24}
-      speed={1000}
-      init={false}
-      navigation={true}
-      ref={swiperElRef}
-    >
-      {products}
-    </swiper-container>
+    <>
+      <div className="swiper-navigation">
+        <button
+          type="button"
+          className={
+            isActiveIdx > 0 || isActiveIdx === -1
+              ? "active-swiper-arrow"
+              : undefined
+          }
+          onClick={() => swiperElRef.current?.swiper.slidePrev()}
+        >
+          <Icons name="swiper-left-arrow" />
+        </button>
+        <button
+          type="button"
+          className={isActiveIdx === -1 ? undefined : "active-swiper-arrow"}
+          onClick={() => swiperElRef.current?.swiper.slideNext()}
+        >
+          <Icons name="swiper-right-arrow" />
+        </button>
+      </div>
+      <swiper-container
+        slides-per-view={slidesPerView}
+        speed={1000}
+        init={false}
+        ref={swiperElRef}
+      >
+        {products}
+      </swiper-container>
+    </>
   );
 };
