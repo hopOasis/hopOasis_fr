0c1c9bfcfe190a0389835e9e1bb283b583183821
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-29 11:34:04 +0200

    add logo

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,10 +3,14 @@ import { LogoLink } from "../components/ui/links/links";
 export default function Page() {
   return (
     <>
-      <header className="container">
-        <LogoLink />
+      <header className="header">
+        <div className="container">
+          <LogoLink variant="dark"/>
+        </div>
       </header>
-      <main className="container">Checkout-page</main>
+      <main className="container">
+        <form>Checkout-page</form>
+      </main>
     </>
   );
 }
