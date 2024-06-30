52c81180a2ef3b3950775a923419ea6b91c71d51
Slava <110696118+sl7one@users.noreply.github.com>
2024-06-30 21:12:49 +0200

    add fetching weakProducts section

--- app/components/containers/weeklyProducts/weakProducts.tsx
+++ app/components/containers/weeklyProducts/weakProducts.tsx
@@ -1,19 +1,16 @@
-import { beer } from "@/app/static/bear";
 import Section from "../../ui/section/section";
 import "./weakProducts.scss";
 import { CardSlider } from "../../ui/slider/CardSlider";
 import Card from "../../ui/card/Card";
 import { memo } from "react";
+import { ShopCardType } from "@/app/shop/types";
+import { getData } from "@/app/api/api";
+import { Endpoints } from "@/app/api/types";
 
-
-
-const WeakProducts = memo(() => {
-  const products = Array(8)
-    .fill(1)
-    .map(() => {
-      const id = crypto.randomUUID();
-      return { ...beer, id };
-    });
+const WeakProducts = memo(async () => {
+  const products: ShopCardType[] = await getData({
+    endpoint: Endpoints.beer,
+  });
   return (
     <Section>
       <p className="title">Товари тижня</p>
