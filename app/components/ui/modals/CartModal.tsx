b47244445e5d071c64dc89d36f7e297335c48d24
Slava <110696118+sl7one@users.noreply.github.com>
2024-07-01 11:29:38 +0200

    add overflow

--- app/components/ui/modals/CartModal.tsx
+++ app/components/ui/modals/CartModal.tsx
@@ -4,10 +4,12 @@ import ModalTop from "./ModalTop";
 import WeeklyProductsModal from "./WeeklyProductsModal";
 import { IPropsCartModal } from "./types";
 import CartList from "../../containers/CartList/CartList";
+import Overflow from "../Overflow";
 
 export default  function CartModal({ cart, products }: IPropsCartModal) {
   return (
     <dialog open className="cart-modal">
+      <Overflow />
       <ModalTop />
       <CartList cart={cart} />
       <WeeklyProductsModal products={products} />
