a0268978393641751611cd8162b60fbfdc27a74c
Slava <110696118+sl7one@users.noreply.github.com>
2024-07-01 15:05:50 +0200

    add animation

--- app/components/ui/modals/CartModal.tsx
+++ app/components/ui/modals/CartModal.tsx
@@ -5,11 +5,11 @@ import WeeklyProductsModal from "./WeeklyProductsModal";
 import { IPropsCartModal } from "./types";
 import CartList from "../../containers/CartList/CartList";
 import Overflow from "../Overflov/Overflow";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { animate } from "@/app/utils";
 
 export default function CartModal({ cart, products }: IPropsCartModal) {
-  useEffect(() => {
+  useLayoutEffect(() => {
     const animation = animate.modal.open();
     return () => animation.revert();
   }, []);
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -4,16 +4,21 @@ export const parseProductName = (name: string) => name.split(" ").join("_");
 
 export const animate = {
   modal: {
-    tl: gsap.timeline(),
-    open: function () {
-      //   const newTl = gsap.timeline();
-      this.tl
-        .to("dialog", { top: 100, opacity: 1, duration: 0.2 })
-        .to("dialog", { top: 0, duration: 0.35 });
-      return this.tl;
+    open: () => {
+      const tl = gsap.timeline();
+      tl.to("dialog", { top: 100, opacity: 1, duration: 0.2 }).to("dialog", {
+        top: 0,
+        duration: 0.35,
+      });
+      return tl;
     },
-    close: function ({ cb }) {
-      return this.tl.reverse().callbackScope("onComplete", cb);
+    close: ({ cb }: { cb: () => void }) => {
+      gsap.to("dialog", {
+        top: -500,
+        opacity: 0,
+        duration: 0.2,
+        onComplete: cb,
+      });
     },
   },
 };
