2ca4ff916c65c9e6deb07178c27f1ec6851ac4a1
Slava <110696118+sl7one@users.noreply.github.com>
2024-07-15 21:08:53 +0200

    ref about-page list

--- app/about/components/ProductionProccesSection.tsx
+++ app/about/components/ProductionProccesSection.tsx
@@ -1,4 +1,5 @@
 import Section from "@/app/components/ui/section/section";
+import { aboutPageList } from "@/app/static/aboutPageList";
 import Image from "next/image";
 
 export default function ProductionProccesSection() {
@@ -16,39 +17,9 @@ export default function ProductionProccesSection() {
         </div>
         <div className="about-page__right-block">
           <ul className="production-procces-list">
-            <li>
-              Збір і підготовка сировини: Ячмінь мочать, сушать та калібрують.
-              Хміль розсипають та розмелюють. Вода піддається очищенню та
-              фільтрації.
-            </li>
-            <li>
-              Збереження сировини: Після підготовки сировину зберігають у
-              відповідних умовах для збереження якості.
-            </li>
-            <li>
-              Варіння сусла: Сировину сумішують у воді та підвищують температуру
-              для видобуття цукрів.
-            </li>
-            <li>
-              Додавання хмелю: Додають хміль для надання пиву гіркоти, аромату
-              та консервації.
-            </li>
-            <li>
-              Фільтрування: Сусло фільтрують для видалення залишків сировини.
-            </li>
-            <li>
-              Бродіння: Додають дріжджі для бродіння цукрів у алкоголь та
-              вуглекислоту.
-            </li>
-            <li>
-              Дозрівання: Після бродіння пиво дозріває для покращення смакових
-              якостей.
-            </li>
-
-            <li>
-              Розлив: Готове пиво розливають у пляшки, банки або бочки для
-              подальшого розподілу.
-            </li>
+            {aboutPageList.map((el, idx) => (
+              <li key={idx}>{el}</li>
+            ))}
           </ul>
         </div>
       </div>
