f51fb2817e02252a79e4ce216063dd7df4290558
Slava <110696118+sl7one@users.noreply.github.com>
2024-07-18 15:21:20 +0200

    add throttling

--- app/checkout/components/deliveryRadioComponents.tsx
+++ app/checkout/components/deliveryRadioComponents.tsx
@@ -2,6 +2,7 @@
 import { IPropsDepartmentComponent } from "../types";
 import AsyncSelect from "react-select/async";
 import { getNewPostSettlementsLib } from "@/app/api/api";
+import { throttle } from "throttle-debounce";
 const defaultOption = [
   {
     label: "Почніть вводти назву міста. Викоистовуйте українську літерацію",
@@ -12,14 +13,26 @@ export const DepartmentComponent = ({
   isTrueCurrentLocation,
   value,
 }: IPropsDepartmentComponent) => {
+  const throttledFunc = throttle(
+    1000,
+    async (city: string, resolve: (val: any) => void) => {
+      const { data } = await getNewPostSettlementsLib({ city });
+      if (!data.success) return resolve(defaultOption);
+      const options = data.data[0].Addresses.map(({ Present }) => ({
+        value: Present,
+        label: Present,
+      }));
+      resolve(options);
+    },
+    { noLeading: true }
+  );
+
   const promiseOptions = async (city: string) => {
     if (!city) return defaultOption;
-    const { data } = await getNewPostSettlementsLib({ city });
-    if (!data.success) return defaultOption;
-    const options = data.data[0].Addresses.map(({ Present }) => ({
-      value: Present,
-      label: Present,
-    }));
+
+    const options = await new Promise((resolve) => {
+      throttledFunc(city, resolve);
+    });
 
     return options;
   };
