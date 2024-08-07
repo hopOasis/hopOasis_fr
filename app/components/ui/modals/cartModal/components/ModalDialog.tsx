b0033d3f5cc78fecf86bb24bfbdfedbe3fea6b30
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-08-07 16:00:05 +0300

    fix more mistakes

--- app/components/ui/modals/cartModal/components/ModalDialog.tsx
+++ app/components/ui/modals/cartModal/components/ModalDialog.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
-import SpesialForYouModal from "./SpesialForYouModal";
-import { animate } from "@/app/utils";
-import Overflow from "../../../Overflov/Overflow";
-import CartList from "@/app/components/containers/CartList/CartList";
-import ModalTop from "./ModalTop";
-import { IPropsCartModal } from "../types";
+import CartList from '@/app/components/containers/CartList/CartList';
+import { animate } from '@/app/utils';
+import { useEffect } from 'react';
+import Overflow from '../../../Overflov/Overflow';
+import { IPropsCartModal } from '../types';
+import ModalTop from './ModalTop';
+import SpesialForYouModal from './SpesialForYouModal';
 
 export default function ModalDialog({ cart, products }: IPropsCartModal) {
   useEffect(() => {
     const animation = animate.modal.open();
-    return () => animation.revert();
+    return () => {
+      animation.revert();
+    };
   }, []);
   return (
     <dialog open className="cart-modal">
