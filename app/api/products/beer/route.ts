f4218fcabf2de35027cad984a941f8cef7f26ca7
Slava <110696118+sl7one@users.noreply.github.com>
2024-08-14 14:41:10 +0200

    Update route.ts

--- app/api/products/beer/route.ts
+++ app/api/products/beer/route.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Endpoints, ProductsResponseType } from '../../types';
+import { NextRequest, NextResponse } from "next/server";
+import { Endpoints, ProductsResponseType } from "../../types";
 
 export async function GET() {
   const resProducts = await fetch(process.env.API_URL + Endpoints.beer);
   if (!resProducts.ok) {
-    console.log('error', resProducts);
-    throw new Error('Failed to fetch data');
+    console.log("error", resProducts);
+    throw new Error("Failed to fetch data");
   }
 
   const parsedRes: ProductsResponseType = await resProducts.json();
@@ -15,12 +15,13 @@ export async function GET() {
 
 export async function POST(request: NextRequest) {
   const resProduct = await fetch(process.env.API_URL + Endpoints.beer, {
-    method: 'POST',
+    method: "POST",
     body: request.body,
   });
 
   if (!resProduct.ok) {
-    throw new Error('Failed to fetch data');
+    console.log("error", resProduct);
+    throw new Error("Failed to fetch data");
   }
 
   const product = await resProduct.json();
