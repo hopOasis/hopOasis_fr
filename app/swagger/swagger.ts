f2d8e8dc572377e7d49d9b3e9a94129ad97f198b
Slava <110696118+sl7one@users.noreply.github.com>
2024-08-22 22:38:50 +0200

    Update swagger.ts

--- app/swagger/swagger.ts
+++ app/swagger/swagger.ts
@@ -15,61 +15,245 @@ export const getApiDocs = async () => {
         '/beers': {
           get: {
             tags: ['Products'],
-            description: 'Getting beers from remote api',
-            responses: {},
+            description: 'Getting BEERS from remote api and convert them to unifiable objects',
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: [
+                      {
+                        id: 'BEER-30001',
+                        name: 'Beer name',
+                        volumeLarge: 0.5,
+                        volumeSmall: 0.33,
+                        priceLarge: 150,
+                        priceSmall: 100,
+                        description: 'Beer description',
+                        beerColor: 'white',
+                        image: ['https://image-name'],
+                        rating: 4.3,
+                        votes: 40,
+                        specialOfferIds: [1],
+                        itemType: 'BEER',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
           },
         },
         '/beers/{id}': {
           get: {
             tags: ['Products'],
-            description: 'Getting beer from remote api',
+            description: 'Getting BEER from remote api and convert to unifiable object',
             parameters: [{ name: 'id', in: 'path', required: true, type: 'number' }],
-            responses: {},
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: {
+                      id: 'BEER-30001',
+                      name: 'Beer name',
+                      volumeLarge: 0.5,
+                      volumeSmall: 0.33,
+                      priceLarge: 150,
+                      priceSmall: 100,
+                      description: 'Beer description',
+                      beerColor: 'white',
+                      image: ['https://image-name'],
+                      rating: 4.3,
+                      votes: 40,
+                      specialOfferIds: [1],
+                      itemType: 'BEER',
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         '/ciders': {
           get: {
             tags: ['Products'],
-            description: 'Getting ciders from remote api',
-            responses: {},
+            description: 'Getting CIDERS from remote api and convert them to unifiable objects',
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: [
+                      {
+                        id: 'CIDER-30001',
+                        name: 'Cider name',
+                        volumeLarge: 0.5,
+                        volumeSmall: 0.33,
+                        priceLarge: 150,
+                        priceSmall: 100,
+                        description: 'Cider description',
+                        beerColor: null,
+                        image: ['https://image-name'],
+                        rating: 4.3,
+                        votes: 40,
+                        specialOfferIds: [1],
+                        itemType: 'CIDER',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
           },
         },
         '/ciders/{id}': {
           get: {
             tags: ['Products'],
-            description: 'Getting cider from remote api',
+            description: 'Getting CIDER from remote api convert to unifiable object',
             parameters: [{ name: 'id', in: 'path', required: true, type: 'number' }],
-            responses: {},
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: {
+                      id: 'CIDER-30001',
+                      name: 'Cider name',
+                      volumeLarge: 0.5,
+                      volumeSmall: 0.33,
+                      priceLarge: 150,
+                      priceSmall: 100,
+                      description: 'Cider description',
+                      beerColor: null,
+                      image: ['https://image-name'],
+                      rating: 4.3,
+                      votes: 40,
+                      specialOfferIds: [1],
+                      itemType: 'CIDER',
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         '/snacks': {
           get: {
             tags: ['Products'],
-            description: 'Getting snacks from remote api',
-            responses: {},
+            description: 'Getting SNACKS from remote api and convert them to unifiable objects',
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: [
+                      {
+                        id: 'SNACK-30001',
+                        name: 'Snack name',
+                        volumeLarge: 0.5,
+                        volumeSmall: 0.33,
+                        priceLarge: 150,
+                        priceSmall: 100,
+                        description: 'Snack description',
+                        beerColor: null,
+                        image: ['https://image-name'],
+                        rating: 4.3,
+                        votes: 40,
+                        specialOfferIds: [1],
+                        itemType: 'SNACK',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
           },
         },
         '/snacks/{id}': {
           get: {
             tags: ['Products'],
-            description: 'Getting snack from remote api',
+            description: 'Getting SNACK from remote api api and convert to unifiable object',
             parameters: [{ name: 'id', in: 'path', required: true, type: 'number' }],
-            responses: {},
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: {
+                      id: 'SNACK-30001',
+                      name: 'Snack name',
+                      volumeLarge: 0.5,
+                      volumeSmall: 0.33,
+                      priceLarge: 150,
+                      priceSmall: 100,
+                      description: 'Snack description',
+                      beerColor: null,
+                      image: ['https://image-name'],
+                      rating: 4.3,
+                      votes: 40,
+                      specialOfferIds: [1],
+                      itemType: 'SNACK',
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         '/sets': {
           get: {
             tags: ['Products'],
-            description: 'Getting sets from remote api',
-            responses: {},
+            description: 'Getting SETS from remote api and convert them to unifiable objects',
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: [
+                      {
+                        id: 'PRODUCT_BUNDLE-30001',
+                        name: 'Set name',
+                        volumeLarge: 300,
+                        volumeSmall: 150,
+                        priceLarge: 150,
+                        priceSmall: 100,
+                        description: 'Set description',
+                        beerColor: null,
+                        image: ['https://image-name'],
+                        rating: 4.3,
+                        votes: 40,
+                        specialOfferIds: [1],
+                        itemType: 'PRODUCT_BUNDLE',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
           },
         },
         '/sets/{id}': {
           get: {
             tags: ['Products'],
-            description: 'Getting set from remote api',
+            description: 'Getting set from remote api and convert to unifiable object',
             parameters: [{ name: 'id', in: 'path', required: true, type: 'number' }],
-            responses: {},
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: {
+                      id: 'PRODUCT_BUNDLE-30001',
+                      name: 'Set name',
+                      volumeLarge: 300,
+                      volumeSmall: 150,
+                      priceLarge: 150,
+                      priceSmall: 100,
+                      description: 'Set description',
+                      beerColor: null,
+                      image: ['https://image-name'],
+                      rating: 4.3,
+                      votes: 40,
+                      specialOfferIds: [1],
+                      itemType: 'PRODUCT_BUNDLE',
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         '/weekProducts': {
@@ -113,14 +297,65 @@ export const getApiDocs = async () => {
                 },
               },
             },
-            responses: {},
+            responses: {
+              200: {
+                description: 'The response returns a list of BEERS | CIDERS | SNACKS | SETS by filter',
+              },
+            },
+          },
+        },
+        '/products/{id}': {
+          post: {
+            tags: ['Service'],
+            description: 'Getting PRODUCT by filter',
+            parameters: [{ name: 'id', in: 'path', required: true, type: 'number' }],
+            requestBody: {
+              description:
+                "value can be      all | beers | ciders | snacks | sets . By defaulf ruturns data from '/beers' endpoint.",
+              required: true,
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      filter: {
+                        type: 'string',
+                        enum: ['all', 'beer', 'cider', 'snacks', 'sets'],
+                        description: 'Filter for the products',
+                      },
+                    },
+                    required: ['filter'],
+                  },
+                  example: {
+                    filter: '=> enum  all | BEER | CIDER | SNACK | PRODUCT_BUNDLE',
+                  },
+                },
+              },
+            },
+
+            responses: {
+              200: {
+                description: 'The response returns an object of BEERS | CIDERS | SNACKS | SETS by filter',
+              },
+            },
           },
         },
         '/location': {
           get: {
             tags: ['Service'],
-            description: 'Getting cart from remote api',
-            responses: {},
+            description: 'Getting LOCATION from remote api',
+            responses: {
+              200: {
+                content: {
+                  'application/json': {
+                    example: {
+                      ip: '37.73.207.179',
+                      city: 'Kiev',
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         '/carts': {
