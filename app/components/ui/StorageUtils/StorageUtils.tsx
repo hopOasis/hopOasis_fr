b5278ce77e788f93ea8f075fdec18c2a78349a3a
Slava <110696118+sl7one@users.noreply.github.com>
2024-09-21 11:53:21 +0200

    fixes

--- app/components/ui/StorageUtils/StorageUtils.tsx
+++ app/components/ui/StorageUtils/StorageUtils.tsx
@@ -6,13 +6,10 @@ import { useEffect } from 'react';
 
 export default function StorageUtils() {
   useEffect(() => {
-<<<<<<< Updated upstream
-=======
     const saveCookiesToDatabase = async () => {
       await fetch(ProxiEndpoints.dbSaveCookie, { method: 'GET', credentials: 'include' });
     };
     saveCookiesToDatabase();
->>>>>>> Stashed changes
     window.addEventListener('beforeunload', () => oazaStorage.clearSecure());
   }, []);
   return null;
